1c6ceb9514f335fac55dca4b67f3349bebf8e31e
alvindav <alvind@sfu.ca>
2018-03-17 03:35:58 -0700

    Added skeleton Crop and Farm classes

---
+++ android/app/src/main/java/com/example/alvin/farming/Farm.java
@@ -0,0 +1,15 @@
+package com.example.alvin.farming;
+
+/**
+ * Created by Alvin on 2018-03-17.
+ */
+
+public class Farm {
+    Crop cropArray; //how to make this an array?
+
+    /*insert methods*/
+    //access crop info (use getFunctions)
+    //reset water timer -- alter a second database?
+    //add to cropArray
+    //remove from cropArray
+}
