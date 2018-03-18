ffffb6bc1a3bf95ab9acbe5b33ce7713d2a251b3
alvindav <alvind@sfu.ca>
2018-03-18 02:04:00 -0700

    add button lol

--- android/app/src/main/java/com/verysadengineers/farming/printCrop.java
+++ android/app/src/main/java/com/verysadengineers/farming/printCrop.java
@@ -3,7 +3,9 @@ package com.verysadengineers.farming;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,19 +31,32 @@ public class printCrop extends AppCompatActivity {
     private ListView cropClimates;
     private ListView cropSeasons;
 
+    private Button addButton;
+
     private List<String> listClimates;
     private List<String> listSeasons;
 
     private TextView bio;
 
     private DatabaseReference databaseDescription;
+    private DatabaseReference databaseMyCrop;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_print_crop);
-        Crop crop = (Crop) getIntent().getSerializableExtra("crop");
+        final Crop crop = (Crop) getIntent().getSerializableExtra("crop");
         databaseDescription = FirebaseDatabase.getInstance().getReference("description");
+        databaseDescription = FirebaseDatabase.getInstance().getReference("mycrop");
 
+        addButton = findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                addCrop(crop);
+            }
+
+        });
 
         cropName = (TextView) findViewById(R.id.nameText);
         cropName.setText(crop.getName());
@@ -49,8 +64,8 @@ public class printCrop extends AppCompatActivity {
         cropHarvest = (TextView) findViewById(R.id.harvestText);
         cropHarvest.setText(""+ crop.getHarvestTime() +" days to grow");
 
-        cropHarvest = (TextView) findViewById(R.id.wateringText);
-        cropHarvest.setText("Water every "+ crop.getWaterTimer() +" hours");
+        cropWatering = (TextView) findViewById(R.id.wateringText);
+        cropWatering.setText("Water every "+ crop.getWaterTimer() +" hours");
 
         cropClimates = findViewById(R.id.climatesText);
         cropSeasons = findViewById(R.id.seasonsText);
@@ -72,5 +87,10 @@ public class printCrop extends AppCompatActivity {
         cropSeasons.setAdapter(seasonAdapter);
 
 
+    }
+    private void addCrop(Crop crop){
+        String id = databaseMyCrop.push().getKey();
+        Crop newCrop = new Crop( crop.getName(), crop.getSeason(), crop.getClimate(), crop.getHarvestTime(), crop.getImageURL());
+        databaseMyCrop.child(id).setValue(newCrop);
     }
 }
